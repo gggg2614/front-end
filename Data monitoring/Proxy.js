1f99e82b98ebaf8ea5c81730d25fbeb77b279357
gggg2614 <839857519@qq.com>
2022-08-14 21:44:12 +0800

    readme

---
+++ Data monitoring/Proxy.js
@@ -0,0 +1,34 @@
+// var people = { name: '123', age: 10 }
+
+// var interceptors = {
+//     set: function (receiver, property, value) {
+//         console.log(receiver);
+//         console.log(property);
+//         console.log(value);
+//         receiver[property] = value
+//     },
+//     get:function(target,prop,receiver) {
+//         console.log(target);
+//         console.log(prop);
+//         console.log(receiver);
+//        return 101   
+//     }
+// }
+
+// people = new Proxy(people, interceptors)
+
+// people.name=11
+
+//单继承
+// var foo = {
+//     pname: 1,
+//     foo: function () {
+//         console.log('foo1');
+//     }
+// }
+// var sonoffoo = Object.create(foo)
+// sonoffoo.foo()
+
+//多继承
+
+
